d135dcd8428bfc0e9be608b7b22c2cfa470d3bbb
Clemens Elflein <clemens1@familie-elflein.de>
2022-03-09 17:44:36 +0100

    redirect_serial.sh now redirects all motor serials to tcp port 2101, 2102 and 2103

--- utils/redirect_serial.sh
+++ utils/redirect_serial.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-/usr/bin/socat -b128 -d -d TCP-LISTEN:2102,fork,reuseaddr FILE:$1,b115200,raw
+trap 'kill %1; kill %2' SIGINT
+/usr/bin/socat -b128 -d -d TCP-LISTEN:2101,fork,reuseaddr FILE:/dev/ttyAMA1,b115200,raw &
+/usr/bin/socat -b128 -d -d TCP-LISTEN:2102,fork,reuseaddr FILE:/dev/ttyAMA2,b115200,raw &
+/usr/bin/socat -b128 -d -d TCP-LISTEN:2103,fork,reuseaddr FILE:/dev/ttyAMA3,b115200,raw
