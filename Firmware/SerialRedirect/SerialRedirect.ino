042521caf1884f82cbde27896606dd7dca58e95e
ClemensElflein <clemens1@familie-elflein.de>
2022-04-11 00:28:14 +0200

    added prebuilt SerialRedirect.uf2

--- Firmware/SerialRedirect/SerialRedirect.ino
+++ Firmware/SerialRedirect/SerialRedirect.ino
@@ -6,16 +6,29 @@ void setup() {
   Serial.begin(115200);
   Serial1.begin(115200);
 
-  // Enable Raspi
   pinMode(21, OUTPUT);
+  // Disable Raspi
+  digitalWrite(21, LOW);
+
+  // Wait for Serial to be available
+  while(!Serial);
+  delay(1000);
+
+  // Drain serial input
+  while(Serial.available())
+    Serial.read();
+
+  do {
+    Serial.println("Send anything to start Raspi");
+    delay(1000);
+  } while(!Serial.available());
+
+  // Drain serial input again
+  while(Serial.available())
+    Serial.read();
+
+  // Enable Raspi
   digitalWrite(21, HIGH);
-  // Enable ESCs
-  pinMode(20, OUTPUT);
-  digitalWrite(20, HIGH);
-  // Enable GPS
-  pinMode(22, OUTPUT);
-  digitalWrite(22, HIGH);
-  
 }
 
 void loop() {
