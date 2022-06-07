5b55ae273da72e053c99e790fcd676b547f69f1d
Clemens Elflein <clemens1@familie-elflein.de>
2022-06-07 02:47:53 +0200

    fixed scripts

--- utils/scripts/start_openocd.sh
+++ utils/scripts/start_openocd.sh
@@ -3,8 +3,6 @@
 # This script starts OpenOCD on the pi and allows external connections.
 # This way you can use VS code to remotely debug the Pico code.
 
-echo "out" > /sys/class/gpio/gpio10/direction
-echo "1" > /sys/class/gpio/gpio10/value
 echo "10" > /sys/class/gpio/export
 echo "out" > /sys/class/gpio/gpio10/direction
 echo "1" > /sys/class/gpio/gpio10/value
