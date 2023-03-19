ff77e80c3be35f59dd395b6e0db102f6b36aa3cc
clemens <clemens@pop-os.localdomain>
2023-03-19 17:25:14 +0100

    feat: Added 0.12 hardware support

---
+++ Firmware/LowLevel/src/imu/LSM6DSO/imu.cpp
@@ -0,0 +1,49 @@
+#include "imu.h"
+#include "pins.h"
+#include <LSM6DSOSensor.h>
+#include <PioSPI.h>
+
+PioSPI spiBus(PIN_IMU_MOSI, PIN_IMU_MISO, PIN_IMU_SCK, PIN_IMU_CS, SPI_MODE3, 1000000);
+LSM6DSOSensor IMU(&spiBus, PIN_IMU_CS, 1000000);
+int32_t accelerometer[3];
+int32_t gyroscope[3];
+
+bool init_imu()
+{
+    spiBus.begin();
+    int status = IMU.begin();
+    if (status != 0)
+        return false;
+
+    if (IMU.Enable_G() != 0)
+        return false;
+
+    if (IMU.Enable_X() != 0)
+        return false;
+    return true;
+}
+
+bool imu_read(float *acceleration_mss, float *gyro_rads, float *mag_uT)
+{
+    bool success = true;
+    success &= IMU.Get_X_Axes(accelerometer) == 0;
+    success &= IMU.Get_G_Axes(gyroscope) == 0;
+
+    acceleration_mss[0] = accelerometer[0];
+    acceleration_mss[1] = accelerometer[1];
+    acceleration_mss[2] = accelerometer[2];
+
+    gyro_rads[0] = gyroscope[0];
+    gyro_rads[1] = gyroscope[1];
+    gyro_rads[2] = -gyroscope[2];
+
+    mag_uT[0] = 0;
+    mag_uT[1] = 0;
+    mag_uT[2] = 0;
+
+    return success;
+}
+
+void imu_loop()
+{
+}
